06e7ff8ec18a6b3f48d208c682b92660ae718251
oceaster <oceaster@gmail.com>
2020-03-15 18:57:33 +0000

    v1.2.4

--- reacts/src/index.js
+++ reacts/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
-import App from './js/App';
+import App from './js/homepage';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ reacts/src/js/homepage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../css/header.css';
+import page_banner from './components/PageBanner';
+
+function App() {
+  return page_banner();
+}
+
+export default App;
