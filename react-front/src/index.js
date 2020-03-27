dfbf5ba8619f2ae2facdeb991d6ac1f3d4fa87c5
oceaster <oceaster@gmail.com>
2020-03-27 10:39:13 +0000

    test

--- react-front/src/index.js
+++ react-front/src/index.js
@@ -7,30 +7,28 @@ ReactDOM.render(<App />, document.getElementById('root'));
 
 
 async function catchPostData() {
-    
-    const response = await fetch("/api?q=hello_world&r=posts");
-    var my_data  = await response.text();    
-    my_data = my_data.split("<br>");
-    
-    var posts = [];
-    var post_data = [];
-    
-    for (var p=0;p<my_data.length-1;p++) {
+    const post_data = [];
+    const api_data  = await fetch("/api?q=hello_world&r=posts");
+
+    for (let p=0;p<api_data.length-1;p++) {
         post_data.push(
             [
-                my_data[p].split(" ")[0],
-                my_data[p].split(" ")[1],
-                my_data[p].split(" ")[2]
+                api_data[p].split(" ")[0],
+                api_data[p].split(" ")[1],
+                api_data[p].split(" ")[2]
             ]
         );
-    }
+    }    
     
-    for (p=0;p<post_data.length;p++) {
+    const posts = [];
+    
+    for (let p=0;p<post_data.length;p++) {
         const post_signature = React.createElement('p', {}, post_data[p][1]);
         const post_content = React.createElement('p', {}, post_data[p][2]);
         posts.push(post_signature);
         posts.push(post_content);
     }
+    return posts
 }
 
 
