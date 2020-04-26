bbaf899c9850bf94834f8b016ca0ef49f32a54a6
oceaster <oceaster@gmail.com>
2020-04-26 22:16:03 +0100

    js overhaul

--- templates/pages/error/error.js
+++ templates/pages/error/error.js
@@ -1,4 +1,4 @@
-// ALERT USER OF ERROR ON PAGE
+/* ALERT USER OF ERROR ON PAGE */
 alert( "sorry! this page is currently unavailable." );
-// REDIRECT USER BACK TO SAFETY
+/* REDIRECT USER BACK TO SAFTEY */
 window.location.href = "/";
